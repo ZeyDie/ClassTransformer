d44d0a3c291486301a6f49db341d79737e4f515c
Zoom4ikDan4ik <zoom4ikdan4ik@gmail.com>
2020-05-23 13:55:42 +0300

    Initial commit

---
+++ src/main/java/ru/zoom4ikdan4ik/transformer/Tweaker.java
@@ -0,0 +1,28 @@
+package ru.zoom4ikdan4ik.transformer;
+
+import net.minecraft.launchwrapper.ITweaker;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+
+import java.io.File;
+import java.util.List;
+
+public class Tweaker implements ITweaker {
+    @Override
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
+    }
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+        classLoader.registerTransformer(Transformer.class.getName());
+    }
+
+    @Override
+    public String getLaunchTarget() {
+        return "net.minecraft.client.main.Main";
+    }
+
+    @Override
+    public String[] getLaunchArguments() {
+        return new String[0];
+    }
+}
